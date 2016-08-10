b510a2beb221fe6900830ba953d99625eaf804fe
Nathan Rajlich <nathan@tootallnate.net>
2016-08-10 13:01:13 -0700

    add "own" module root lookup logic
    So if you invoke the module with no arguments,
    then the module root of the file that is invoking
    the function will be returned. i.e.
    
    ``` js
    var root = moduleRoot();
    ```

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
  */
 
 var path = require('path');
+var callsite = require('callsite');
 var findup = require('findup-sync');
 
-
 module.exports = function(name) {
+  if (!name) {
+    name = callsite()[1].getFileName();
+  }
   var fullpath = path.dirname(require.resolve(name));
   var dir = findup('package.json', {cwd: fullpath})
   return path.resolve(path.dirname(dir));
